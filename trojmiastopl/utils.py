8d0a9eb2c64b25300d1a79c84c81474b77e85356
Arkadiusz Klein <arek.klein@limebrains.com>
2017-08-10 11:20:55 +0200

    Temporary

--- trojmiastopl/utils.py
+++ trojmiastopl/utils.py
@@ -65,6 +65,8 @@ def get_url_for_filters(payload):
     """
     response = requests.post(SEARCH_URL, payload, headers={'User-Agent': get_random_user_agent()})
     html_parser = BeautifulSoup(response.content, "html.parser")
+    if html_parser is None:
+        raise requests.HTTPError
     url = html_parser.find(class_="nice-select-tsi").find("option").next_sibling.next_sibling.attrs["value"]
     return url
 
